3803fdd0e1e2d770f67e466185f2a3f942971d7e
joshuacitrak <joshuacitrak@gmail.com>
2016-01-05 14:30:50 -0800

    animation tweaks

--- 120x240/_/components/js/main.js
+++ 120x240/_/components/js/main.js
@@ -3,9 +3,9 @@ function playAd(){
     stl.from('#bradSvg1Container', 15, {scale:1.5, x:-100, y:-80, rotationX:20, rotationY:30, rotationZ:-20, force3D:true})
        .to("#bradSvg2Container", 3, {rotationY:20, x:-110, y: 50, scale:1, force3D:true}, 0)
        .to("#bradSvg2Container", 3, {rotationY:0,  x:-130, y: 0, scale:1.3, force3D:true}, 3)
-        .to("#bradSvg2Container", 3, {rotationY:20,  x:-150, scale:1, z:0,force3D:true}, 6)
-        .to("#bradSvg2Container", 3, {rotationY:0,  x:-130, y:-50 }, 9)
-        .to("#bradSvg2Container", 3, {opacity:0, scale:1.5, rotationY:-20,  x:-150,ease: Power3.easeOut }, 12);
+        .to("#bradSvg2Container", 3, {rotationY:20,  x:-120, scale:1, z:0,force3D:true}, 6)
+        .to("#bradSvg2Container", 1.5, {rotationY:0,  x:-100, scale:1.2 ,y:-50 }, 9)
+        .to("#bradSvg2Container", 3, {opacity:0, scale:1.5, rotationY:-20,  x:-150,ease: Power3.easeOut }, 10.5);
     
     var ttl = new TimelineLite();
     ttl.from("#bradT1", .8, {y:-60, opacity:0,ease: Power3.easeOut}, 0)
@@ -29,14 +29,14 @@ function playAd(){
     
     var ctl = new TimelineLite();
         ctl.from("#bradKidney", 3, {opacity:0, scale:0,x:20, ease: Power2.easeOut})
-       .from("#bradFlare", 1.5, {x:-120, opacity:0,ease: Power3.easeOut}, "-=1.6")
-       .from("#bradCtaContainer", .8, {clip:"rect(0px 0px 60px 0px)", opacity:0,ease: Power3.easeOut}, "-=1.6")
+       .from("#bradFlare", 1.5, {x:-120, opacity:0,ease: Power3.easeOut}, "-=1")
+       .from("#bradCtaContainer", .8, {clip:"rect(0px 0px 105px 0px)", opacity:0,ease: Power3.easeOut}, "-=1.5")
        .from("#bradCta", .8, {opacity:0, ease: Power3.easeOut}, "-=.8")
        .from("#bradKidneyFlare", 1.5, {opacity:0, ease: Power3.easeOut}, "-=.6");
     
     tl.add(stl, 0)
     .add(ttl, 0)
-    .add(ctl, 11.5);
+    .add(ctl, 10.5);
     tl.totalDuration(15);
        
 };
@@ -65,7 +65,7 @@ function checkInit() {
                function onInit() {
                      CSSPlugin.defaultTransformPerspective = 750;
     TweenLite.set("#bradContainer", {opacity:1});
-    TweenLite.set('#bradSvg1Container', {rotationX:-55,rotationY:-20, rotationZ:-15, force3D:true});
+    TweenLite.set('#bradSvg1Container', {rotationX:35, rotationZ: -30, x:-60, y:-35, force3D:true, scale:.7});
     TweenLite.set("#bradSvg2Container", { scale:.6, rotation:180, x:-150, y:-50, opacity:.8});
     addListeners();
     playAd();
